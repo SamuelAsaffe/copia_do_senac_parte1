e53fdafc83a2031380407309b722ca68c9668a13
SamuelAsaffe <samuelasaffe1@gmail.com>
2023-06-20 13:13:48 -0300

    a

--- js/script.js
+++ js/script.js
@@ -57,65 +57,68 @@ function medianotas() {
     let n1 = parseFloat(document.getElementById("nota1").value)
     let n2 = parseFloat(document.getElementById("nota2").value)
     let n3 = parseFloat(document.getElementById("nota3").value)
-    let media = (n1 + n2 + n3) / 3
+    media = (n1 + n2 + n3) / 3
     document.getElementById("medianota").innerText = ("A média da sua nota é: " + media.toFixed(1))
 
 
 }
 
-totalPresencas= 0
-ct=0
+totalPresencas = 0;
+ct = 0;
+let media = 0;
+let perpresenca = 0;
 function checkPresenca(presencaId, faltaId) {
     var presencaRadioButton = document.getElementById(presencaId);
     var faltaRadioButton = document.getElementById(faltaId);
-    document.getElementById("msgpresenca").innerText= ("")
-    
+    document.getElementById("msgpresenca").innerText = ("")
 
     if (presencaRadioButton.checked) {
         faltaRadioButton.checked = false // Desmarca o radio button de falta
         totalPresencas++
-        
-        if (totalPresencas > 12){
+        if (totalPresencas > 12) {
             totalPresencas = 0
             ct++
-            document.getElementById("msgpresenca").innerText= ("para de tentar trapacear e faz direito, ja foi "+ct+" tentativa(s)")
-            if (ct >= 5){
-            
-                document.getElementById("msgpresenca").innerText= ("Tu é percistenbte em")
-                
+            document.getElementById("msgpresenca").innerText = ("para de tentar trapacear e faz direito, ja foi " + ct + " tentativa(s)")
+            if (ct >= 5) {
+                document.getElementById("msgpresenca").innerText = ("Tu é percistenbte em")
             }
         }
-        
-        document.getElementById("ttlpresenca").innerText= totalPresencas
+        document.getElementById("ttlpresenca").innerText = totalPresencas
         // ppresenca=((100/12)*totalpresenca)
         // document.getElementById("presenca").innerText= (totalPresencas+'% de presença')
-        perpresenca =((100/12)*totalPresencas).toFixed(1)
-        document.getElementById("ppresenca").innerText= (perpresenca +"% de presença")
+        perpresenca = ((100 / 12) * totalPresencas).toFixed(1)
+        document.getElementById("ppresenca").innerText = (perpresenca + "% de presença")
     }
-    
-    
+
 
 }
 
 function checkFalta(presencaId, faltaId) {
     var presencaRadioButton = document.getElementById(presencaId)
     var faltaRadioButton = document.getElementById(faltaId)
-    
 
     if (faltaRadioButton.checked) {
         presencaRadioButton.checked = false // Desmarca o radio button de presença
         totalPresencas--
-        if (totalPresencas < 0){
+        if (totalPresencas < 0) {
             totalPresencas = 0
         }
-        document.getElementById("ttlpresenca").innerText= totalPresencas
-        perpresenca =((100/12)*totalPresencas).toFixed(1)
-        document.getElementById("ppresenca").innerText= (perpresenca +"% de presença")    }
-}
-function verifCOndi() {
-if (perpresenca >=75) {
-    if (media >= 6) {
-        
+        document.getElementById("ttlpresenca").innerText = totalPresencas
+        perpresenca = ((100 / 12) * totalPresencas).toFixed(1)
+        document.getElementById("ppresenca").innerText = (perpresenca + "% de presença")
     }
 }
+function verifyCondi() {
+    if (perpresenca >= 75) {
+        if (media >= 6) {
+            document.getElementById("saidaVerifyCond").innerText = ("aprovado por ter nota e persencas suficientes")
+        } else if ((media >= 5) && (media <= 6)) {
+            document.getElementById("saidaVerifyCond").innerText = ("Recuperação por ter presenças o suficiente mas não ter as devidas notas")
+            } else if(media < 5) {
+            document.getElementById("saidaVerifyCond").innerText = ("reprovado por não ter notas suficientes")
+            }
+    }else {
+        document.getElementById("saidaVerifyCond").innerText = ("Reprovado por não ter presenças o suficiente")
+    }
+    
 }
